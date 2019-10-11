c41ba91d697a784783af69440dc0ce106cdac221
Gary Liang <gary.liang@nab.com.au>
2019-10-11 11:05:40 +1100

    set up basic date

--- src/App.js
+++ src/App.js
@@ -1,69 +1,68 @@
-import React from "react";
-import FileViewer from 'react-file-viewer';
-import { CustomErrorComponent } from 'custom-error';
-import fileUrl from './test_pdf.pdf';
-
-/*
-FileViewer supported type
-----------------
-Images: png, jpeg, gif, bmp, including 360-degree images
-pdf
-csv
-xslx
-docx
-Video: mp4, webm
-Audio: mp3
-
-*/
-
-export function convertDataToBinary(base64Data) {
-  // win.atob === raw char
-  const raw = window.atob(base64Data);
-  // arr[unit_8][buffer]
-  const array = new Uint8Array(new ArrayBuffer(raw.length));
-
-  // arr[0]['a']
-  // arr[1]['b']
-  for (let i = 0; i < raw.length; i++) {
-    array[i] = raw.charCodeAt(i);
-  }
-
-  // re
-  return array;
-}
-
-// return blob
-export function convertDataToBlob(data, mimeType) {
-  // 1. data === base64
-  // 2. mineType === application/pdf
-  const arr = convertDataToBinary(data);
-  // blob([2d_arr], mime_type)
-  return new Blob([arr], {type: mimeType});
-}
+import React, {useState, useRef} from "react";
 
 function App() {
-  //const fileUrl = 'http://www.pdf995.com/samples/pdf.pdf';
+  const prevDateFromRef = useRef('');
+  const [fromDate, setFromDate] = useState('');
 
-  
+  const checkDateValue = (str, max) => {
+    if (str.charAt(0) !== '0' || str === '00') {
+      let num = parseInt(str);
 
+      if (isNaN(num) || num <= 0 || num > max) num = 1;
 
-  //const file = require('./file.json');
-  const mimeType = 'pdf';
-  //const data = convertDataToBlob(file, mimeType);
-  //const tmpPath = window.URL.createObjectURL(data);
+      str =
+        num > parseInt(max.toString().charAt(0)) && num.toString().length === 1
+          ? '0' + num // self, no more #
+          : num.toString(); // can be more num
+    }
+    return str;
+  };
 
-  const onError = (e) => {
-    console.error(e);
+  // user input
+  const formatInputDate = (input) => {
+    // from date
+    if (
+      prevDateFromRef.current.length > input.length &&
+      /\d\/$/.test(prevDateFromRef.current)
+    ) {
+      input = input.substr(0, input.length - 1);
+    }
+    
+
+    let values = input.split('/').map(value => {
+      return value.replace(/\D/g, '');
+    });
+
+    // day
+    if (values[0]) values[0] = checkDateValue(values[0], 31);
+
+    // month
+    if (values[1]) values[1] = checkDateValue(values[1], 12);
+
+    let output = values.map((value, index) => {
+      return value.length === 2 && index < 2 ? value + '/' : value;
+    });
+
+    let assignVal = output.join('').substr(0, 10);
+    prevDateFromRef.current = assignVal;
+    
+    return assignVal;
   }
 
   return <div className="App">
-    <FileViewer
-      fileType={mimeType}
-      //filePath={tmpPath}
-      filePath={fileUrl}
-      errorComponent={CustomErrorComponent}
-      onError={onError}/>
-    );
+    <input
+      id="fromDate"
+      placeholder={'DD/MM/YYYY'}
+      onChange={event => {
+        // output date
+        let outputDate = formatInputDate(event.target.value);
+   
+        // set from date val
+        setFromDate(outputDate);
+      }}
+      value={fromDate}
+  
+    />
   </div>;
 }
 
